d3de030fc1e6d907363cfc87690a787cabdb3500
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2021-11-11 11:48:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['https://chat.whatsapp.com/FDnHRof3iIV1MfoW5vlMKQ', 'https://chat.whatsapp.com/FDnHRof3iIV1MfoW5vlMKQ']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '6282328303332': { // put your number here
+  '6285731855426': { // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
     isDev: true, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
+  '6285731855426': { // put your number here
     name: 'Dave',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '60199782326': {
-    name: 'Adiixyz',
+  '6285731855426': {
+    name: 'Han',
     isReport: true, // if true this number will send if user use command /report or /join
   },
-  '6285713964963': {
+  '6285731855426': {
     isReport: true
   },
-  '13479805233': {} // ignore if you don't want anything, like name dll
+  '6285731855426': {} // ignore if you don't want anything, like name dll
 }
-global.mods = ['13479805233'] // Want some help?
+global.mods = ['6285731855426'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
-global.botwm = 'RPG BOT'
+global.botwm = 'HanRpg'
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -47,8 +47,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'GAME BOT WHATSAPP'
+global.packname = 'wa.me/6285731855426'
+global.author = 'HanRpg'
 
 global.multiplier = 39 // The higher, The harder levelup
 
